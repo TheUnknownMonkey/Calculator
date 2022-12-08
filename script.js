6c8e9a73829bfae5440bccfa5d0d214c94d7733b
TheUnknownMonkey <miha.puc@gmail.com>
2022-12-08 21:32:58 +0100

    styling done

---
+++ script.js
@@ -0,0 +1,35 @@
+const acButton = document.getElementById("button-AC");
+const negativeButton = document.getElementById("button-negative");
+const procentageButton= document.getElementById("button-procentage");
+const divideButton = document.getElementById("button-divide");
+const multiplyButton = document.getElementById("button-multiply");
+const minusButton = document.getElementById("button-minus");
+const dividePlus = document.getElementById("button-plus");
+const divideEquals = document.getElementById("button-equals");
+
+const buttonOne = document.getElementById("button-1");
+const buttonTwo = document.getElementById("button-2");
+const buttonThree = document.getElementById("button-3");
+const buttonFour = document.getElementById("button-4");
+const buttonFive = document.getElementById("button-5");
+const buttonSix = document.getElementById("button-6");
+const buttonSeven = document.getElementById("button-7");
+const buttonEight = document.getElementById("button-8");
+const buttonNine = document.getElementById("button-9");
+const buttonZero = document.getElementById("button-0");
+
+const display = document.getElementById("display");
+
+let initialInput = true;
+let result = 0;
+newCalculation = true;
+let number1 = 0;
+let number2 = 0;
+white (newCalculation==true){
+    
+    if (initialInput==true){
+
+    }
+
+}
+
